b56abe6891ff543c7387ee6c9e4c90097ce22ecc
T-Visor <turkimbro@yahoo.com>
2024-09-17 20:12:43 -0400

    modified homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,7 +4,11 @@ const HomePage = () => (
     <div className="space-y-6">
         <h2 className="text-3xl font-bold">Welcome to My Portfolio</h2>
         <p className="text-lg">
-            Explore my works with Artificial Intelligence and Photograph.
+        I am thrilled by the incredible potential of generative AI, both today and in the future!
+        The possibilities are endless, and I am excited to be part of this transformative journey!
+        </p>
+        <p className="text-lg">
+        In addition you can occassionally find me behind the camera, capturing the beauty of the world as a passionate part-time photographer!
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="bg-gray-700 p-4 rounded-lg">
