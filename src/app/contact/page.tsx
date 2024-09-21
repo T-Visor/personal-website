f20ed6cccc8bab0dabf4e6a078449289b2df2467
T-Visor <turkimbro@yahoo.com>
2024-09-21 10:00:24 -0400

    finished refactoring contact page for maintainability

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,91 +2,75 @@
 import { Alert, AlertDescription } from "@/components/ui/alert"
 import { Bold } from "lucide-react";
 
-type ContactEntry = {
+type ContactListItem = {
     id: number;
     label: string;
     rawURL: string;
     anchorText: string;
 }
 
-const contactListItems: ContactEntry[] = [
+const contactListItems: ContactListItem[] = [
     {
         id: 1,
-        label: "Email:",
+        label: "Email",
         rawURL: "mailto:turkimbro@gmail.com",
         anchorText: "turkimbro@gmail.com"
     },
     {
         id: 2,
-        label: "LinkedIn:",
+        label: "LinkedIn",
         rawURL: "https://linkedin.com/in/turhan-kim",
         anchorText: "LinkedIn Profile"
     },
     {
         id: 3,
-        label: "IEEE:",
+        label: "IEEE",
         rawURL: "https://ieeexplore.ieee.org/author/37089429839",
         anchorText: "IEEE Profile"
     },
     {
         id: 4,
-        label: "GitHub:",
+        label: "GitHub",
         rawURL: "https://github.com/T-Visor",
         anchorText: "GitHub Profile"
     },
     {
         id: 5,
-        label: "EyeEm:",
+        label: "EyeEm",
         rawURL: "https://www.eyeem.com/u/turkimbro/photos",
         anchorText: "EyeEm Profile"
     },
 ]
 
-type ContactEntryProps = {
+type ContactListItemProps = {
     label: string;
     rawURL: string;
     anchorText: string;
 };
 
-//const SectionCard: React.FC<ContactEntryProps> = ({ label, rawURL, anchorText }) => (
+const SectionCard: React.FC<ContactListItemProps> = ({ label, rawURL, anchorText }) => (
+    <p><strong>{label + ": "} </strong>
+        <a href={rawURL} target="_blank" className="text-blue-400 hover:underline">
+            {anchorText}
+        </a>
+    </p>
+);
 
-//);
-
-const ContactPage = () => (
+const ContactPage: React.FC = () => (
     <div className="space-y-6">
         <h2 className="text-3xl font-bold">Contact Me</h2>
-        
-        {/* Centering and adjusting the width for desktop */}
         <div className="flex justify-left">
             <div className="w-full sm:w-1/2 lg:w-1/2">
                 <Alert>
                     <AlertDescription>
                         <div className="text-lg space-y-2">
-                            <p><strong>Email: </strong>  
-                                <a href="mailto:turkimbro@gmail.com" target="_blank" className="text-blue-400 hover:underline">
-                                    turkimbro@gmail.com
-                                </a>
-                            </p>
-                            <p><strong>LinkedIn: </strong> 
-                                <a href="https://linkedin.com/in/turhan-kim" target="_blank" className="text-blue-400 hover:underline">
-                                    LinkedIn Profile
-                                </a>
-                            </p>
-                            <p><strong>IEEE: </strong> 
-                                <a href="https://ieeexplore.ieee.org/author/37089429839" target="_blank" className="text-blue-400 hover:underline">
-                                    IEEE Xplore Profile
-                                </a>
-                            </p>
-                            <p><strong>GitHub: </strong> 
-                                <a href="https://github.com/T-Visor" target="_blank" className="text-blue-400 hover:underline">
-                                    GitHub Profile
-                                </a>
-                            </p>
-                            <p><strong>EyeEm: </strong> 
-                                <a href="https://www.eyeem.com/u/turkimbro/photos" target="_blank" className="text-blue-400 hover:underline">
-                                    EyeEm Profile
-                                </a>
-                            </p>
+                            {contactListItems.map((contactListItem) =>
+                                <SectionCard
+                                    label={contactListItem.label}
+                                    rawURL={contactListItem.rawURL}
+                                    anchorText={contactListItem.anchorText}
+                                />
+                            )}
                         </div>
                     </AlertDescription>
                 </Alert>
