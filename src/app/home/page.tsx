bd42c6916dab83bab24a6168a707d62c4b9eafef
Turhan Akira <tkimbr1@students.towson.edu>
2024-09-29 19:03:29 -0400

    updated landing page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,14 +4,12 @@ const HomePage: React.FC = () => (
   <div className="space-y-6">
     <h2 className="text-3xl font-bold">Welcome To My Portfolio</h2>
     <p className="text-lg">
-      I am thrilled by the incredible potential of artificial intelligence (AI) technology! I have experience in computer vision
-      and natural language processing.
-
-      In particular, I have used convolutional neural networks (CNNs) and 
-      generative pre-trained transformers (GPTs) for research and prototyping. 
+      I am passionate about the transformative possibilities of artificial intelligence (AI) technology. 
+      With hands-on experience in both computer vision and natural language processing, 
+      I have worked extensively with advanced models such as convolutional neural networks (CNNs) and generative pre-trained transformers (GPTs) for research and prototyping.
     </p>
     <p className="text-lg">
-      In addition, you&apos;ll occassionally find me behind the camera as a passionate part-time photographer!
+      In addition to my work in AI, I am also a passionate part-time photographer, often found behind the camera capturing moments of creativity and inspiration.
     </p>
   </div>
 )
