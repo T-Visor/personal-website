54f683e4295a882f4472a594e1c5a35aef8ea476
T-Visor <turkimbro@yahoo.com>
2024-10-06 11:41:07 -0400

    modified dimensions for fullscreen photo view

--- src/app/photography/page.tsx
+++ src/app/photography/page.tsx
@@ -19,8 +19,8 @@ const images: galleryImage[] = [
     url: "https://res.cloudinary.com/dfiwecvfd/image/upload/v1727647259/bridge-view-min_lgvapr.jpg",
     height: 500,
     width: 500,
-    fullscreenHeight: 700,
-    fullscreenWidth: 700,
+    fullscreenHeight: 500,
+    fullscreenWidth: 500,
     alt: "Image with sky in background."
   },
   {
@@ -28,8 +28,8 @@ const images: galleryImage[] = [
     url: "https://res.cloudinary.com/dfiwecvfd/image/upload/v1727646361/8FEFF362-BA95-4BBB-80C9-5CDC6DCC254D_mcqet9.jpg",
     height: 500,
     width: 500,
-    fullscreenHeight: 700,
-    fullscreenWidth: 700,
+    fullscreenHeight: 800,
+    fullscreenWidth: 800,
     alt: "Image with sky in background."
   },
   {
@@ -37,8 +37,8 @@ const images: galleryImage[] = [
     url: "https://res.cloudinary.com/dfiwecvfd/image/upload/v1727646361/IMG_0781_bnry3a.jpg",
     height: 500,
     width: 500,
-    fullscreenHeight: 700,
-    fullscreenWidth: 700,
+    fullscreenHeight: 800,
+    fullscreenWidth: 800,
     alt: "Image with sky in background."
   },
   {
@@ -46,8 +46,8 @@ const images: galleryImage[] = [
     url: "https://res.cloudinary.com/dfiwecvfd/image/upload/v1727646360/IMG_1148_yaegg8.jpg",
     height: 500,
     width: 500,
-    fullscreenHeight: 700,
-    fullscreenWidth: 700,
+    fullscreenHeight: 800,
+    fullscreenWidth: 800,
     alt: "Image with sky in background."
   },
   {
@@ -55,8 +55,8 @@ const images: galleryImage[] = [
     url: "https://res.cloudinary.com/dfiwecvfd/image/upload/v1727646361/cabin-view_qumj6w.jpg",
     height: 500,
     width: 500,
-    fullscreenHeight: 700,
-    fullscreenWidth: 700,
+    fullscreenHeight: 800,
+    fullscreenWidth: 800,
     alt: "Image with sky in background."
   },
   {
@@ -64,8 +64,8 @@ const images: galleryImage[] = [
     url: "https://res.cloudinary.com/dfiwecvfd/image/upload/v1727646361/C6015A3C-9653-4BEA-96C6-7E93327A9209_jitmqp.jpg",
     height: 500,
     width: 500,
-    fullscreenHeight: 700,
-    fullscreenWidth: 700,
+    fullscreenHeight: 500,
+    fullscreenWidth: 500,
     alt: "Image with sky in background."
   },
 ]
@@ -129,6 +129,7 @@ const PhotographyPage = () => {
             src={fullScreenImage.url}
             width={fullScreenImage.fullscreenWidth} 
             height={fullScreenImage.fullscreenHeight}  
+            crop="fit" 
             alt={fullScreenImage.alt}  
             className="object-contain"
           />
